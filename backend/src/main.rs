ae2d270fb19fe77e45acbcea72035a4e4dfe1e51
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-12-21 11:54:27 +0100

    initial commit with basic axum backend

---
+++ backend/src/main.rs
@@ -0,0 +1,13 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let port = std::env::var("PORT").unwrap_or("3000".to_string());
+    let addr = format!("localhost:{port}");
+    println!("listening on http://{addr}");
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
